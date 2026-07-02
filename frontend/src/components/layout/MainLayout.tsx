788185a7ba7fab37d7bde66e760f304908f46622
renVScode <renindino02@gmail.com>
2026-07-02 23:46:01 +0800

    side bar adjustments

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -12,7 +12,7 @@ export function MainLayout() {
         <div className="fixed inset-0 bg-black/40 z-30 lg:hidden" onClick={() => setSidebarOpen(false)} />
       )}
       <div className={`fixed inset-y-0 left-0 z-40 w-64 transform transition-transform duration-200 ease-in-out
-        ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:relative lg:translate-x-0`}>
+        ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} lg:relative lg:translate-x-0 lg:h-full`}>
         <Sidebar onClose={() => setSidebarOpen(false)} />
       </div>
       <div className="flex-1 flex flex-col min-w-0">
